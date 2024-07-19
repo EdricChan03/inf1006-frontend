25dcdbd1ce70d27f34a80bf15896ad43a0a58943
Edric <edric.chan.1997@gmail.com>
2024-07-19 15:53:57 +0800

    refactor(+page): move actions dashboard card to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 
   import StatusChip from '$lib/components/status/StatusChipContent.svelte';
 
+  import ActionsDashboardCard from '$lib/ui/actions/ActionsDashboardCard.svelte';
   import SensorDashboardCard from '$lib/ui/sensors/SensorDashboardCard.svelte';
 
   import { flyAndScale } from '$lib/utils/transitions/fly-and-scale';
@@ -133,18 +134,10 @@
     temperatureData={$temperatureSensor}
     humidityData={$humiditySensor}
   />
+
   <!-- Actions -->
-  <section class="rounded bg-purple-200 p-3 flex flex-col gap-3 border border-purple-400">
-    <header>
-      <h2 class="text-2xl">Actions</h2>
-    </header>
-    <div class="grid gap-3">
-      <button
-        class="bg-blue-200 hover:bg-blue-300 active:bg-blue-400 rounded shadow border-2 border-blue-400 focus:ring focus-visible:outline-none focus:ring-blue-500 transition-all p-3 flex flex-col gap-2 place-content-center items-center"
-        onclick={onActivateSensorsClick}><CircleGaugeIcon />Activate sensors</button
-      >
-    </div>
-  </section>
+  <ActionsDashboardCard {onActivateSensorsClick} />
+
   <!-- Camera feed -->
   <section
     class="rounded bg-lime-200 p-3 flex flex-col gap-3 border border-lime-400 md:col-span-full"
