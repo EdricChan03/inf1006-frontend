7a990f94165ce3aa91c26bc47ceb0525b8c2f743
Edric <edric.chan.1997@gmail.com>
2024-07-26 13:02:14 +0800

    chore(SettingsDialogContent): add step + fix min value

--- src/lib/ui/settings/SettingsDialogContent.svelte
+++ src/lib/ui/settings/SettingsDialogContent.svelte
@@ -31,7 +31,8 @@
       <input
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 transition-all"
         type="number"
-        min="3"
+        min="3000"
+        step="100"
         bind:value={tempSettings.intervalMs}
         required
       />
